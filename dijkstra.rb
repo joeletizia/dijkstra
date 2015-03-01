8e032480fe0d627fd05ed2ee3c1bac2894891a5c
Joe Letizia <joe@gust.com>
2015-03-01 13:26:52 -0500

    Complete

--- dijkstra.rb
+++ dijkstra.rb
@@ -9,7 +9,7 @@ class Edge
   attr_accessor :destination, :weight
 end
 
-lines = File.open("./small_test.txt", "r").readlines
+lines = File.open("./dijkstraData.txt", "r").readlines
 
 edges = {}
 
@@ -28,82 +28,59 @@ lines.each do |line|
   end
 end
 
-def min_distance_destination(edges, source, available_nodes)
-  place_to_go = nil
-  min = nil
-  edges.each do |edge|
-    if edge.v1 == source
-      destination = edge.v2
-      if available_nodes.include? destination
-        place_to_go = destination if min.nil? || min > edge.weight
-      end
-    end
+def vertex_with_min_distance(edges, distances, unvisited)
+  min = Float::INFINITY
+  node_to_visit = nil
 
-    if edge.v2 == source
-      destination = edge.v1
-      if available_nodes.include? destination
-        place_to_go = destination if min.nil? || min > edge.weight
-      end
+  unvisited.each do |unvisited_node|
+    if distances[unvisited_node] < min
+      min = distances[unvisited_node] 
+      node_to_visit = unvisited_node
     end
   end
 
-  place_to_go
-end
-
-def distance_between(edges, source, destination)
-  edges[source].each do |edge|
-    return edge.weight if edge.destination == destination
-  end
-
-  raise "There is no connection between the two nodes #{source} and #{destination}"
+  node_to_visit
 end
 
 def dijkstra(edges, source)
   distances = {}
-  edges.keys.each do |key|
-    distances[key] = Float::INFINITY
-  end
+  previous = {}
+  unvisited = []
+
   distances[source] = 0
+  previous[source] = nil
 
-  visited = []
-  unvisited = edges.keys
+  edges.each do |node, edge_array|
+    unless source == node
+      distances[node] = Float::INFINITY
+      previous[node] = nil
+    end
 
-  current_node = source
-  current_traveled_distance = 0
-  traveled_path = {}
-  current_traveled_path = []
+    unvisited.push node
+  end
 
-  binding.pry
+  u = source
 
   while unvisited.any?
-    neighbor_edges = edges[current_node]
+    u = vertex_with_min_distance(edges, distances, unvisited)
+    puts "#{u} is closest. Deleteing #{u}"
+    unvisited.delete(u)
 
-    node_to_move_to = nil
-    min_travel_distance_to_next_node = Float::INFINITY
-
-    neighbor_edges.each do |neighbor_edge|
-      if neighbor_edge.weight < min_travel_distance_to_next_node && unvisited.include?(neighbor_edge.destination)
-        min_travel_distance_to_next_node = neighbor_edge.weight
-        node_to_move_to = neighbor_edge.destination
-      end
-
-      if distances[neighbor_edge.destination] > neighbor_edge.weight + current_traveled_distance
-        distances[neighbor_edge.destination] = current_traveled_distance + neighbor_edge.weight
-        traveled_path[neighbor_edge.destination] = current_traveled_path + [current_node, neighbor_edge.destination]
+    puts "#{unvisited.count} left unvisited"
+    if edges[u]
+      edges[u].each do |edge|
+        alt = distances[u] + edge.weight
+        if alt < distances[edge.destination]
+          distances[edge.destination] = alt
+          previous[edge.destination] ||= []
+          previous[edge.destination] << u
+        end
       end
     end
 
-    binding.pry
-
-    visited << current_node
-    current_traveled_path << current_node
-    unvisited.delete(current_node)
-
-    break if min_travel_distance_to_next_node == Float::INFINITY
-    current_traveled_distance += edges[current_node].select{|node| node.destination == node_to_move_to}.first.weight
-    current_node = node_to_move_to
   end
-  [distances, traveled_path]
+
+  return [distances, previous]
 end
 
 def output_format(distances_hash, paths_hash)
@@ -114,7 +91,7 @@ def output_format(distances_hash, paths_hash)
 end
 
 def big_output_format(distances_hash, paths_hash)
-  nodes_to_output = [10,30,50,80,90,110,130,160,180,190]
+  nodes_to_output = [7,37,59,82,99,115,133,165,188,197]
 
   nodes_to_output.each do |node|
     path = node ? paths_hash[node] - [1]: []
@@ -122,8 +99,18 @@ def big_output_format(distances_hash, paths_hash)
   end
 end
 
+def big_output_format_dist(distances_hash, paths_hash)
+  nodes_to_output = [7,37,59,82,99,115,133,165,188,197]
+
+  nodes_to_output.each do |node|
+    path = node ? paths_hash[node] - [1]: []
+    puts "#{node} #{distances_hash[node]}"
+  end
+end
+
+
 
 x = dijkstra(edges, 1)
 binding.pry
-output_format(x.first, x[1])
+big_output_format_dist(x.first, x[1])
 binding.pry
